dc350d87aeec233d5ba9b44f861406f3c4ee3635
crow18120 <crow18120@gmail.com>
2022-10-24 18:13:11 +0700

    [AnhnQ] update ...

--- Problem Solving (Intermediate)/Medium/fraudulent-activity-notifications.py
+++ Problem Solving (Intermediate)/Medium/fraudulent-activity-notifications.py
@@ -1,17 +1,31 @@
-def pos(d):
+def posMedian(d):
     return [int(d/2)] if bool(d % 2) else [int(d/2) - 1, int(d/2)]
 
 
 def initializeSet(expenditure, d):
-    return expenditure[:d].sort()
+    sortSet = expenditure[:d]
+    sortSet.sort()
+    return sortSet
 
 
 def genNewSet(sortData, dequeueEle, enqueueEle):
     isDequeue = isEnqueue = False
+    newSortData = []
+    for val in sortData:
+        if val == dequeueEle and not isDequeue:
+            isDequeue = True
+            continue
+        if val >= enqueueEle and not isEnqueue:
+            isEnqueue = True
+            newSortData.append(enqueueEle)
+        newSortData.append(val)
+    if not isEnqueue:
+        newSortData.append(enqueueEle)
+
+    return newSortData
 
 
-def median(sortData):
-    pos = pos(len(sortData))
+def median(sortData, pos):
     return sum(sortData[i] for i in pos)/len(pos)
 
 
@@ -19,9 +33,13 @@ def activityNotifications(expenditure, d):
     # Write your code here
     r = 0
     dataSet = initializeSet(expenditure, d)
+    pos = posMedian(d)
     for i in range(d, len(expenditure)):
-        if expenditure[i] >= median(dataSet):
+        if expenditure[i] >= median(dataSet, pos) * 2:
             r += 1
+        dataSet = genNewSet(dataSet, expenditure[i - d], expenditure[i])
+
+    return r
 
 
-
+print(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5))
